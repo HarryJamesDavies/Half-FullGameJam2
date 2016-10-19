c418ed35ff26d26bfafe303c5aebffd4e7d94a39
MattWY <matthew.wynter@btinternet.com>
2016-10-19 18:13:37 +0100

    Added CameraFollow.cs for camera control

---
+++ FISHJam/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+    float m_zoomFactor = 0.3f;
+    float m_movementFactor = 0.1f;
+    public float m_zoomInFactor = 2.0f;
+    public float m_zoomOutFactor = 8.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.mousePosition.x > 0.0f)
+        {
+            transform.position = new Vector3(transform.position.x + m_movementFactor, transform.position.y, transform.position.z);            
+		}
+
+        //to zoom the camera out
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y + m_zoomFactor, transform.position.z);
+            //chehck its y position
+            if (transform.position.y > m_zoomOutFactor)
+            {
+                //then lock its position
+                transform.position = new Vector3(transform.position.x, m_zoomOutFactor, transform.position.z);
+            }
+        }
+        if (Input.mousePosition.x < Screen.width)
+        {
+			transform.position = new Vector3 (transform.position.x - m_movementFactor, transform.position.y, transform.position.z);
+        }
+
+        //to zoom the camera in
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y - m_zoomFactor, transform.position.z);
+            //check its y position
+            if(transform.position.y < m_zoomInFactor)
+            {
+                //then lock its position
+                transform.position = new Vector3(transform.position.x, m_zoomInFactor, transform.position.z);
+            }
+        }
+        if (Input.mousePosition.y > 0.0f)
+        {
+			transform.position = new Vector3 (transform.position.x, transform.position.y, transform.position.z + m_movementFactor);
+		}
+		if (Input.mousePosition.y < Screen.height)
+        {
+			transform.position = new Vector3 (transform.position.x, transform.position.y, transform.position.z - m_movementFactor);
+		}
+	}
+}
