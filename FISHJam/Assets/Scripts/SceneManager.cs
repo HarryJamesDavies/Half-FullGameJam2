08c1c77d06596d2dcd30346bc46f6dc79525e180
Adster94 <adam.moorey@hotmail.co.uk>
2016-10-19 17:27:48 +0100

    Uploaded basic splash screen scenes, and basic scripts for both the Game Manager and Scene Manager. I have also created a Game Manager object that will not get destroyed whenever the scene changes - this should be used for any scripts that you need running all the time.

---
+++ FISHJam/Assets/Scripts/SceneManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneManager : MonoBehaviour {
+
+	public void PlayClick()
+    {
+        Application.LoadLevel(1);
+    }
+
+    public void InstructionsClick()
+    {
+        Application.LoadLevel(2);
+    }
+
+    public void SettingsClick()
+    {
+        Application.LoadLevel(3);
+    }
+
+    public void QuitClick()
+    {
+        Application.Quit();
+    }
+}
