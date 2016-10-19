5fdadb1cf18134cdbcaee65804acab720b15240a
Adster94 <adam.moorey@hotmail.co.uk>
2016-10-19 17:50:28 +0100

    Moved some files into correct places, added UI to each scene - except DemoScene - to transition between the different scenes.

--- FISHJam/Assets/Scripts/SceneManager.cs
+++ FISHJam/Assets/Scripts/SceneManager.cs
@@ -3,6 +3,11 @@ using System.Collections;
 
 public class SceneManager : MonoBehaviour {
 
+    public void MainMenuClick()
+    {
+        Application.LoadLevel(0);
+    }
+
 	public void PlayClick()
     {
         Application.LoadLevel(1);
