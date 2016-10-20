34fd5bb77a6d3dcd931d30380bbbb8f236cc77bd
MattWY <matthew.wynter@btinternet.com>
2016-10-20 23:37:45 +0100

    Fixes to the Workers and added some desire functionality

--- FISHJam/Assets/Scripts/WorkerManager.cs
+++ FISHJam/Assets/Scripts/WorkerManager.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 
 public class WorkerManager : MonoBehaviour {
 
+    public static WorkerManager worker_instance = null;
+
     [SerializeField]
     public List<GameObject> m_WorkerList;
     public GameObject m_Worker;
@@ -12,31 +14,36 @@ public class WorkerManager : MonoBehaviour {
 
     void Start()
     {
-        PopulateWorkerList();
         StartCoroutine(SpawnAllWorkers());              
     }
 
     // Use this for initialization
     void Awake()
-    {
+    { 
+        if (worker_instance == null)
+        {
+            worker_instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
         m_WorkerList = new List<GameObject>();
     }
 
-    private void PopulateWorkerList()
+    public void AddGameObject(GameObject worker)
     {
-        //fill the list with all enemies
-        for (int i = 0; i < m_currentWorkerPop; i++)
-        {
-            m_WorkerList.Add(m_Worker);
-        }
+       
     }
 
     IEnumerator SpawnAllWorkers()
     {
-        for (int i = 0; i < m_WorkerList.Count; i++)
+        for (int i = 0; i < m_currentWorkerPop; i++)
         {
             //spawn the enemy
-            Instantiate(m_Worker, spawnPoint.position, spawnPoint.rotation);
+           GameObject temp = (GameObject) Instantiate(m_Worker, spawnPoint.position, spawnPoint.rotation);
+           m_WorkerList.Add(temp);
             //And wait 1 second to spawn another
             yield return new WaitForSeconds(1f);
         }
