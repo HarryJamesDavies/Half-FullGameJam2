46bcf5eb31700a3b1dde58427a6d4d5820a65bbe
Adster94 <adam.moorey@hotmail.co.uk>
2016-10-21 12:38:57 +0100

    I have managed to successfully detect and use both PS4 and Xbox controllers for PlayerMovement. Made slight changes to PlayerMovement.cs and GameManager.cs along with the creation of a ControllerManager.cs, which manages whether a controller is plugged in or not.

--- FISHJam/Assets/Scripts/GameManager.cs
+++ FISHJam/Assets/Scripts/GameManager.cs
@@ -3,8 +3,25 @@ using System.Collections;
 
 public class GameManager : MonoBehaviour {
 
+    public static GameManager m_gameManager = null;
+
+    public bool m_useController;
+
     void Awake()
     {
+        m_useController = false;
+        
+        //make sure singleton is properly initialised
+        if (m_gameManager == null)
+        {
+            m_gameManager = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        //sets game manager to not be destroyed when changing scenes
         DontDestroyOnLoad(this);
     }
 }
--- FISHJam/Assets/Scripts/PlayerMovement.cs
+++ FISHJam/Assets/Scripts/PlayerMovement.cs
@@ -6,20 +6,38 @@ public class PlayerMovement : MonoBehaviour
     private Vector3 m_target;
     private float m_screenWidth;
     private float m_screenHeight;
+    private float m_speed;
     private Vector3 m_offset;
 
     void Awake()
     {
+        //initialise variables
         m_screenWidth = Screen.width;
         m_screenHeight = Screen.height;
+        m_speed = 0.1f;
         m_offset = new Vector3(m_screenWidth / 200, 0.0f, m_screenHeight / 200);
     }
 
     void LateUpdate()
     {
-        transform.position = new Vector3(Input.mousePosition.x /100, 0.0f, Input.mousePosition.y / 100);
-        transform.position -= m_offset;
-        transform.position += new Vector3(Camera.main.transform.position.x, 0.0f, Camera.main.transform.position.z);
+        //checks if controller is plugged in and applies correct player movement
+        if (GameManager.m_gameManager.m_useController)
+        {
+            Debug.Log("Controller input");
+
+            //constantly apply joystick axis input to player position
+            transform.position += new Vector3(Input.GetAxis("LeftJoystickX") * m_speed, 0.0f, -Input.GetAxis("LeftJoystickY") * m_speed);
+            //transform.position += new Vector3(Camera.main.transform.position.x, 0.0f, Camera.main.transform.position.z);
+        }
+        else
+        {
+            Debug.Log("Mouse control");
+
+            //apply player position to mouse position and move player with camera
+            transform.position = new Vector3(Input.mousePosition.x / 100, 0.0f, Input.mousePosition.y / 100);
+            transform.position -= m_offset;
+            transform.position += new Vector3(Camera.main.transform.position.x, 0.0f, Camera.main.transform.position.z);
+        }
     }
 }
 
