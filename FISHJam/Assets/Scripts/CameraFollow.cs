76866475a501c1b306fb650f956e1dc4da7e9779
Adster94 <adam.moorey@hotmail.co.uk>
2016-10-21 15:31:27 +0100

    Main character and camera movement, with the left and right analogue sticks (respectively), has been implemented and works fine with both controllers.

--- FISHJam/Assets/Scripts/CameraFollow.cs
+++ FISHJam/Assets/Scripts/CameraFollow.cs
@@ -16,47 +16,117 @@ public class CameraFollow : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-		if (Input.mousePosition.x > 0.0f)
+        //checks if controller is plugged in
+        if (GameManager.m_gameManager.m_useController)
         {
-            transform.position = new Vector3(transform.position.x + m_movementFactor, transform.position.y, transform.position.z);            
-		}
-
-        if (Input.mousePosition.x < Screen.width - 10.0f)
-        {
-            transform.position = new Vector3(transform.position.x - m_movementFactor, transform.position.y, transform.position.z);
-        }
-
-        if (Input.mousePosition.y > 0.0f)
-        {
-            transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + m_movementFactor);
-        }
-
-        if (Input.mousePosition.y < Screen.height)
-        {
-            transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z - m_movementFactor);
-        }
-
-        //to zoom the camera out
-        if (Input.GetKeyDown(KeyCode.Q))
-        {
-            transform.position = new Vector3(transform.position.x, transform.position.y + m_zoomFactor, transform.position.z);
-            //chehck its y position
-            if (transform.position.y > m_zoomOutFactor)
+            switch (GameManager.m_gameManager.m_controllerType)
             {
-                //then lock its position
-                transform.position = new Vector3(transform.position.x, m_zoomOutFactor, transform.position.z);
+                case "Xbox":
+                    //camera moves as the player uses the right stick
+                    transform.position += new Vector3(Input.GetAxis("RightJoystickX(Xbox)") * m_movementFactor, 0.0f,
+                        -Input.GetAxis("RightJoystickY(Xbox)") * m_movementFactor);
+
+                    //zoom in camera
+                    if (Input.GetButtonDown("RightBumper"))
+                    {
+                        transform.position = new Vector3(transform.position.x, transform.position.y - m_zoomFactor, transform.position.z);
+                        //check its y position
+                        if (transform.position.y < m_zoomInFactor)
+                        {
+                            //then lock its position
+                            transform.position = new Vector3(transform.position.x, m_zoomInFactor, transform.position.z);
+                        }
+                    }
+
+                    //zoom out camera
+                    if (Input.GetButtonDown("LeftBumper"))
+                    {
+                        transform.position = new Vector3(transform.position.x, transform.position.y + m_zoomFactor, transform.position.z);
+                        //check its y position
+                        if (transform.position.y > m_zoomOutFactor)
+                        {
+                            //then lock its position
+                            transform.position = new Vector3(transform.position.x, m_zoomOutFactor, transform.position.z);
+                        }
+                    }
+                    break;
+                case "PS4":
+                    //camera moves as the player uses the right stick
+                    transform.position += new Vector3(Input.GetAxis("RightJoystickX(PS4)") * m_movementFactor, 0.0f,
+                        -Input.GetAxis("RightJoystickY(PS4)") * m_movementFactor);
+
+                    //zoom in camera
+                    if (Input.GetButtonDown("R1"))
+                    {
+                        transform.position = new Vector3(transform.position.x, transform.position.y - m_zoomFactor, transform.position.z);
+                        //check its y position
+                        if (transform.position.y < m_zoomInFactor)
+                        {
+                            //then lock its position
+                            transform.position = new Vector3(transform.position.x, m_zoomInFactor, transform.position.z);
+                        }
+                    }
+
+                    //zoom out camera
+                    if (Input.GetButtonDown("L1"))
+                    {
+                        transform.position = new Vector3(transform.position.x, transform.position.y + m_zoomFactor, transform.position.z);
+                        //check its y position
+                        if (transform.position.y > m_zoomOutFactor)
+                        {
+                            //then lock its position
+                            transform.position = new Vector3(transform.position.x, m_zoomOutFactor, transform.position.z);
+                        }
+                    }
+                    break;
+                default:
+                    break;
             }
         }
-
-        //to zoom the camera in
-        if (Input.GetKeyDown(KeyCode.E))
+        else
         {
-            transform.position = new Vector3(transform.position.x, transform.position.y - m_zoomFactor, transform.position.z);
-            //check its y position
-            if(transform.position.y < m_zoomInFactor)
+            if (Input.mousePosition.x > 0.0f)
             {
-                //then lock its position
-                transform.position = new Vector3(transform.position.x, m_zoomInFactor, transform.position.z);
+                transform.position = new Vector3(transform.position.x + m_movementFactor, transform.position.y, transform.position.z);
+            }
+
+            if (Input.mousePosition.x < Screen.width - 10.0f)
+            {
+                transform.position = new Vector3(transform.position.x - m_movementFactor, transform.position.y, transform.position.z);
+            }
+
+            if (Input.mousePosition.y > 0.0f)
+            {
+                transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + m_movementFactor);
+            }
+
+            if (Input.mousePosition.y < Screen.height)
+            {
+                transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z - m_movementFactor);
+            }
+
+            //to zoom the camera out
+            if (Input.GetKeyDown(KeyCode.Q))
+            {
+                transform.position = new Vector3(transform.position.x, transform.position.y + m_zoomFactor, transform.position.z);
+                //check its y position
+                if (transform.position.y > m_zoomOutFactor)
+                {
+                    //then lock its position
+                    transform.position = new Vector3(transform.position.x, m_zoomOutFactor, transform.position.z);
+                }
+            }
+
+            //to zoom the camera in
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                transform.position = new Vector3(transform.position.x, transform.position.y - m_zoomFactor, transform.position.z);
+                //check its y position
+                if (transform.position.y < m_zoomInFactor)
+                {
+                    //then lock its position
+                    transform.position = new Vector3(transform.position.x, m_zoomInFactor, transform.position.z);
+                }
             }
         }
 	}
