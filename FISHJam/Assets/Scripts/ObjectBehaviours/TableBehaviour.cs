2b25e209cec857bb6f9bdec1a4c7c1294a30d69f
Harry Davies <daviesharry1@gmail.com>
2016-10-22 15:05:52 +0100

    Table now toggles between Textures.

--- FISHJam/Assets/Scripts/ObjectBehaviours/TableBehaviour.cs
+++ FISHJam/Assets/Scripts/ObjectBehaviours/TableBehaviour.cs
@@ -5,6 +5,20 @@ public class TableBehaviour : MonoBehaviour
 { 
     private PlayerAbilities.InteractableStates m_state;
     private bool m_toggle = true;
+    private GameObject m_screen;
+    public int m_imageNum = 0;
+    public Texture[] m_textures;
+
+    void Awake()
+    {
+        for (int i = 0; i <= transform.childCount - 1; i++)
+        {
+            if (transform.GetChild(i).tag == "Screen")
+            {
+                m_screen = transform.GetChild(i).gameObject;
+            }
+        }
+    }
 
     void Update ()
     {
@@ -45,12 +59,14 @@ public class TableBehaviour : MonoBehaviour
     {
         if (m_toggle)
         {
-            gameObject.transform.GetChild(3).GetComponent<Renderer>().material.color = Color.blue;
+          // m_screen.GetComponent<Renderer>().material.color = Color.black;
+            m_screen.GetComponent<Renderer>().material.mainTexture = m_textures[1];
             m_toggle = false;
         }
         else
         {
-            gameObject.transform.GetChild(3).GetComponent<Renderer>().material.color = Color.green;
+            //m_screen.GetComponent<Renderer>().material.color = Color.blue;
+            m_screen.GetComponent<Renderer>().material.mainTexture = m_textures[0];
             m_toggle = true;
         }
     }
