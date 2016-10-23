3531259048dd2655c7d84baaa22b301314d71196
Harry Davies <daviesharry1@gmail.com>
2016-10-23 12:13:56 +0100

    Added particle affects and oter tweaks to the water cooler.

--- FISHJam/Assets/Scripts/ObjectBehaviours/TableBehaviour.cs
+++ FISHJam/Assets/Scripts/ObjectBehaviours/TableBehaviour.cs
@@ -2,12 +2,19 @@
 using System.Collections;
 
 public class TableBehaviour : MonoBehaviour
-{ 
+{
+    public Texture[] m_textures;
+
     private PlayerAbilities.InteractableStates m_state;
     private bool m_toggle = true;
     private GameObject m_screen;
-    public int m_imageNum = 0;
-    public Texture[] m_textures;
+    public int m_imageNum = 1;
+    private int m_screenSaverNum = 1;
+    private int m_activeScreen = 0;
+    private bool m_modified = false;
+
+    public float m_waitLength = 2.0f;
+    private float m_changeStart = 0.0f;
 
     void Awake()
     {
@@ -25,8 +32,33 @@ public class TableBehaviour : MonoBehaviour
         if (GetComponent<InteractableBase>().m_active)
         {
             m_state = GetComponent<InteractableBase>().m_state;
+            m_modified = false;
             ChangeBehaviour();
             GetComponent<InteractableBase>().m_active = false;
+        }
+
+        if(m_modified)
+        {
+            if(Time.time - m_changeStart >= m_waitLength)
+            {
+                m_imageNum++;
+                if(m_imageNum >= m_textures.Length)
+                {
+                    m_imageNum = 2;
+                }
+                ChangeTexture(m_imageNum);
+                m_changeStart = Time.time;
+            }
+        }
+        else if(m_toggle && gameObject.GetComponent<InteractableBase>().m_inUse)
+        {
+            m_imageNum = m_activeScreen;
+            ChangeTexture(m_imageNum);
+        }
+        else
+        {
+            m_imageNum = m_screenSaverNum;
+            ChangeTexture(m_imageNum);
         }
     }
 
@@ -52,32 +84,42 @@ public class TableBehaviour : MonoBehaviour
 
     void NormalBehaviour()
     {
-
+        m_activeScreen = 0;
+        m_screenSaverNum = 1;
     }
 
     void ToggleBehaviour()
     {
         if (m_toggle)
         {
-          // m_screen.GetComponent<Renderer>().material.color = Color.black;
-            m_screen.GetComponent<Renderer>().material.mainTexture = m_textures[1];
+            // m_screen.GetComponent<Renderer>().material.color = Color.black;
+            m_imageNum = m_screenSaverNum;
+            ChangeTexture(m_imageNum);
             m_toggle = false;
         }
         else
         {
             //m_screen.GetComponent<Renderer>().material.color = Color.blue;
-            m_screen.GetComponent<Renderer>().material.mainTexture = m_textures[0];
             m_toggle = true;
         }
     }
 
     void SwapBehaviour()
     {
-
+        m_activeScreen = 1;
+        m_screenSaverNum = 0;
     }
 
     void ModifyBehaviour()
     {
+        m_modified = true;
+        m_imageNum = 2;
+        ChangeTexture(m_imageNum);
+        m_changeStart = Time.time;
+    }
 
+    void ChangeTexture(int i)
+    {
+        m_screen.GetComponent<Renderer>().material.mainTexture = m_textures[i];
     }
 }
--- FISHJam/Assets/Scripts/ObjectBehaviours/WaterCoolerBehaviour.cs
+++ FISHJam/Assets/Scripts/ObjectBehaviours/WaterCoolerBehaviour.cs
@@ -7,16 +7,44 @@ public class WaterCoolerBehaviour : MonoBehaviour
     private Animator m_animator;
     private bool m_toggle = false;
 
+    private GameObject m_sprayWater;
+    private GameObject m_floorWater;
+
     void Awake()
     {
         m_animator = GetComponent<Animator>();
+
+        for (int i = 0; i <= transform.childCount - 1; i++)
+        {
+            if (transform.GetChild(i).name == "WaterParticles")
+            {
+                m_sprayWater = transform.GetChild(i).gameObject;
+            }
+            else if(transform.GetChild(i).name == "FloorWaterParticles")
+            {
+                m_floorWater = transform.GetChild(i).gameObject;
+            }
+        }
     }
 
     void Update()
     {
+        if (!m_toggle)
+        {
+            if (gameObject.GetComponent<InteractableBase>().m_inUse)
+            {
+                m_animator.SetBool("m_triggered", true);
+                m_animator.SetBool("m_active", true);
+            }
+            else
+            {
+                m_animator.SetBool("m_triggered", false);
+            }
+        }
+
         if (GetComponent<InteractableBase>().m_active)
         {
-            m_state = GetComponent<InteractableBase>().m_state;
+            ResetBehaviour();
             ChangeBehaviour();
             GetComponent<InteractableBase>().m_active = false;
         }
@@ -44,40 +72,45 @@ public class WaterCoolerBehaviour : MonoBehaviour
 
     void NormalBehaviour()
     {
-        m_animator.SetBool("Active", true);
-        m_animator.SetBool("Swap", false);
+        ResetBehaviour();
     }
 
     void ToggleBehaviour()
     {
-        m_animator.SetBool("Swap", false);
-
-        if (m_animator.GetBool("Active"))
+        if (!m_toggle)
         {
-            m_animator.SetBool("Active", false);
-        }
-        else
-        {
-            m_animator.SetBool("Active", true);
+            ResetBehaviour();
+            m_toggle = true;
+            m_animator.SetBool("m_triggered", false);
         }
     }
 
     void SwapBehaviour()
     {
-        m_animator.SetBool("Active", true);
-
-        if (m_animator.GetBool("Swap"))
+        if (m_animator.GetBool("m_swap"))
         {
-            m_animator.SetBool("Swap", false);
+            ResetBehaviour();
         }
         else
         {
-            m_animator.SetBool("Swap", true);
+            m_animator.SetBool("m_active", true);
+            m_animator.SetBool("m_swap", true);
         }
+
     }
 
     void ModifyBehaviour()
     {
+        m_animator.SetBool("m_spray", true);
+    }
 
+    void ResetBehaviour()
+    {
+        m_state = GetComponent<InteractableBase>().m_state;
+        m_sprayWater.SetActive(false);
+        m_floorWater.SetActive(false);
+        m_animator.SetBool("m_swap", false);
+        m_animator.SetBool("m_spray", false);
+        m_toggle = false;
     }
 }
