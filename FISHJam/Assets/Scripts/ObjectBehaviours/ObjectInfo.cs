e512cae8365525afa8a13dd80705ca6cda4c5239
Harry Davies <daviesharry1@gmail.com>
2016-10-23 14:31:13 +0100

    Objects now have frustration and suspicion link to them

---
+++ FISHJam/Assets/Scripts/ObjectBehaviours/ObjectInfo.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectInfo : MonoBehaviour {
+
+    public InteractableManager.ObjectType m_type;
+    public float[] m_frustration;
+    public float[] m_suspicion;
+
+    public float GetFrustration()
+    {
+        return m_frustration[(int)gameObject.GetComponent<InteractableBase>().m_state];    
+    }
+
+
+    public float GetSuspicion()
+    {
+        return m_suspicion[(int)gameObject.GetComponent<InteractableBase>().m_state];
+    }
+}
